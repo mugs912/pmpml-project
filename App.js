44779f9bf7d6acf7811b9505ce9ae573c40b3ef5
mugs912 <57297026+mugs912@users.noreply.github.com>
2020-03-01 10:20:16 +0530

    Add files via upload
    react native script which will display only clickable route-ids for given source and destination

--- App.js
+++ App.js
@@ -1,32 +1,38 @@
 import React, { Component } from "react";  
-import { StyleSheet, View, TextInput, Text, Alert ,Button, ScrollView} from "react-native";  
+import {TouchableHighlight,TouchableWithoutFeedback, StyleSheet, View, TextInput, Text, Alert ,Button, ScrollView} from "react-native";  
   
 class App extends Component {  
 	constructor(props) {
 		super(props)
 		this.state = {
 			AnswerText:'helloo all',
-			AnswerText1:'hellooo',
+			routeData:[],
+			stopsData:[],
 			TextInputValueSource: '',
 			TextInputValueDestination: '',
+			
 		}
 	}
 
 	GetValueFunction = () =>{
 		let routes=[];
+		let stops=[];
 		this.setState({AnswerText:null});
 		const { TextInputValueSource,TextInputValueDestination }  = this.state ;
 		Alert.alert(TextInputValueSource+" "+TextInputValueDestination);
 		fetch('http://192.168.43.231:5000/routes?src='+TextInputValueSource+'&dest='+TextInputValueDestination)
 		.then((response) => response.json())
     			.then((responseJson) => {
+    //  				console.log(responseJson);
+    //  				this.setState({AnswerText:JSON.stringify(responseJson)});
       				responseJson.map((ele)=>routes.push(ele.route));
+      				responseJson.map((ele)=>stops.push(ele.stops));
       				console.log(routes);
       				console.log("Length of array is:"+routes.length);
       				console.log(responseJson);
-      				this.setState({AnswerText:JSON.stringify(responseJson)});
-      				this.setState({AnswerText1:String(routes)});
-      				
+      				//this.setState({AnswerText:JSON.stringify(responseJson)});
+      				this.setState({routeData:routes});
+    				this.setState({stopsData:stops});
     			})
     		.catch((error) => {
       			console.error(error);
@@ -34,6 +40,10 @@ class App extends Component {
      	}  
   
 	render() {  
+		let cnt=0;
+		let textEles=this.state.routeData.map((r)=> {   return(<TouchableHighlight key={r} onPress={()=>{Alert.alert("You clicked me!"+r)}}>
+      				<Text style={{textAlign:"center",fontSize:20,padding:20,color:'rgb(0,0,255)',backgroundColor: 'rgb(50,150,120)',margin:5}}>{r+":"+ ++cnt}</Text>
+      			</TouchableHighlight>)});
 		return (
 		<ScrollView>  
       			<View style={styles.container}>  
@@ -51,8 +61,11 @@ class App extends Component {
         		/>  
          		<Button title="Find Routes" onPress={this.GetValueFunction} color="#2196F3" />
          		<Text>{this.state.AnswerText}</Text>
-         		<Text>{this.state.AnswerText1}</Text>
-      			</View>
+         		{textEles}
+      			{/*<TouchableHighlight onPress={()=>{Alert.alert("You clicked me!")}}>
+      				<Text style={{textAlign:"center",fontSize:20,padding:50}}>Click me!!</Text>
+      			</TouchableHighlight>*/}
+  		</View>
       		</ScrollView>  
     		);  
   	}  
@@ -60,7 +73,8 @@ class App extends Component {
   
 const styles = StyleSheet.create({  
 	container: {  
-    		flex: 1  
+    		flex: 1,
+    		padding:20  
   	},  
   	textInputStyle: {  
     		borderColor: '#9a73ef',  
