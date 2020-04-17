71b9e3bbf0aeaa2155d6fde25ebcfee4ed260518
mugs912 <57297026+mugs912@users.noreply.github.com>
2020-04-17 15:33:14 +0530

    find direct routes according to arrival time

--- backend.js
+++ backend.js
@@ -13,8 +13,9 @@ const pool=new Pool({
 
 var rid_stops=[];
 var counter=0;
-const stop_query="select stop_name,stop_seq from stops,routes where stops.stop_id in(select stop_id from routes where route_id=$1 and stop_seq>=$2 and stop_seq<=$3)and stops.stop_id=routes.stop_id and routes.stop_seq>=$2 and routes.stop_seq<=$3 and routes.route_id=$1 order by stop_seq;"
-const query="select r1.route_id, r1.stop_seq as R1Stop, r2.stop_seq as R2Stop from routes r1, routes r2 where r1.route_id=r2.route_id and r1.route_id in(select route_id from routes where r2.stop_id in(select stop_id from stops where stop_name=$1)) and r1.stop_id in(select stop_id from stops where stop_name=$2) and r1.stop_seq > r2.stop_seq";
+const stop_query="select stop_name,stop_seq,latitude,longitude from stops,routes where stops.stop_id in(select stop_id from routes where route_id=$1 and stop_seq>=$2 and stop_seq<=$3)and stops.stop_id=routes.stop_id and routes.stop_seq>=$2 and routes.stop_seq<=$3 and routes.route_id=$1 order by stop_seq;"
+
+const query="SELECT a1.route_id,a1.trip_id,a1.stop_id,a1.stop_seq as sourceseq,a1.arrivaltime as sourcetime,a2.stop_id,a2.stop_seq as destseq,a2.arrivaltime as desttime from arrivaltime a1 join arrivaltime a2 on a1.trip_id=a2.trip_id WHERE a1.trip_id in (SELECT DISTINCT arrivaltime.trip_id from arrivaltime where route_id in( select r1.route_id from routes r1, routes r2 where r1.route_id=r2.route_id and r1.route_id in(select route_id from routes where r2.stop_id in(select stop_id from stops where stop_name=$1)) and r1.stop_id in(select stop_id from stops where stop_name=$2) and r1.stop_seq > r2.stop_seq))and a2.trip_id in(SELECT DISTINCT arrivaltime.trip_id from arrivaltime where route_id in( select r1.route_id from routes r1, routes r2 where r1.route_id=r2.route_id and r1.route_id in(select route_id from routes where r2.stop_id in(select stop_id from stops where stop_name=$1)) and r1.stop_id in(select stop_id from stops where stop_name=$2) and r1.stop_seq > r2.stop_seq))and a1.stop_id in(select stop_id from stops where stop_name=$1)and a2.stop_id in(select stop_id from stops where stop_name=$2)and a1.arrivaltime > (select current_time) order by a1.arrivaltime limit 3";
 
 app.get('/routes',(req,response)=>{
 	rid_stops=[];
@@ -28,11 +29,14 @@ app.get('/routes',(req,response)=>{
 		}
 		for(i=0;i<res.rows.length;++i){
 			let rid=res.rows[i].route_id;
-			pool.query(stop_query,[rid,res.rows[i].r2stop,res.rows[i].r1stop],function findStops(error,result){
+			let srctime=res.rows[i].sourcetime;
+			let desttime=res.rows[i].desttime;
+			pool.query(stop_query,[rid,res.rows[i].sourceseq,res.rows[i].destseq],function findStops(error,result){
 				var stops=[];
-				for(j=0;j<result.rows.length;++j)
-					stops.push(result.rows[j].stop_name);
-				rid_stops.push({'route':rid,'stops':stops});
+				for(j=0;j<result.rows.length;++j){
+					stops.push({'stop':result.rows[j].stop_name,'latitude':result.rows[j].latitude,'longitude':result.rows[j].longitude});
+				}
+				rid_stops.push({'route':rid,'stops':stops,'source arrival time':srctime,'destination arrival time':desttime});
 				if(counter==(res.rows.length)-1){
 					response.send(rid_stops);
 					console.log(rid_stops);	
@@ -41,6 +45,6 @@ app.get('/routes',(req,response)=>{
 				counter++;
 			});
 		}
-	})
+	});
 });
 app.listen(5000,()=>console.log('listening on port 5000'));
